d93124bf90da3c09082a0d52c8f47b5886dea8a1
Patrick Pietens <hello@patrickpietens.com>
2016-04-01 13:31:09 +0200

    Renaming package and adding readme

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ function onError() {
 gulp.task('default', () => {
     let myProduction = process.env.NODE_ENV === 'production';
 
-    let mySource = 'src/select.js',
+    let mySource = 'src/queryselector.js',
         myDestination = 'dist';
 
     let myUmdOptions = {
